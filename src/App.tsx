84508f02b484d3a5b27652a1288af031a7c7e4f3
sara.231.pc <sara.231.pc@gmail.com>
2025-05-07 17:11:03 +0600

    fix: qunaity udation usse on cart

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,29 @@
-import { ThemeProvider } from '@shopify/restyle';
-import React, { type ReactElement } from 'react';
+import React, {type ReactElement} from 'react';
+import {NavigationContainer} from '@react-navigation/native';
+import {ThemeProvider} from '@shopify/restyle';
+import {createNativeStackNavigator} from '@react-navigation/native-stack';
 
 import theme from '@/theme';
-
 import 'react-native-gesture-handler';
-import { Text } from './components';
+import HomeScreen from './screen/HomeScreen';
 
-export const App = (): ReactElement => {
+export type RootNavigatorParamList = {
+  Home: undefined;
+};
+
+interface AppProps {}
+
+const Stack = createNativeStackNavigator<RootNavigatorParamList>();
+
+const App = (_props: AppProps): ReactElement => {
   return (
-      <ThemeProvider theme={theme} >
-        <Text>Shadow Ui</Text>
-      </ThemeProvider>
+    <ThemeProvider theme={theme}>
+      <NavigationContainer>
+        <Stack.Navigator screenOptions={{headerShown: false}}>
+          <Stack.Screen name="Home" component={HomeScreen} />
+        </Stack.Navigator>
+      </NavigationContainer>
+    </ThemeProvider>
   );
 };
 
---
+++ src/screen/HomeScreen.tsx
@@ -0,0 +1,18 @@
+import {Header, Screen, Text} from '@/components';
+import useHeader from '@/hooks/useHeader';
+import React from 'react';
+
+const HomeScreen = () => {
+    useHeader(()=><Header >
+        <Header.BackAction />
+        <Header.Content title="hellow" subTitle="John Doe"/>
+        <Header.Action variant="svg" icon="notification" />
+    </Header>);
+  return (
+    <Screen preset="auto" safeAreaEdges={['top']}>
+      <Text>This is home screen</Text>
+    </Screen>
+  );
+};
+
+export default HomeScreen;
